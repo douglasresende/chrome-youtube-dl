d61134fe98e16820db230f44230b887c747f37e1
Douglas Camargo <douglas@maxstudio.com.br>
2014-11-27 14:07:45 -0200

    Adding rails to project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChromeYoutubeDl::Application.config.secret_key_base = 'a9d2a83553a63a307595ba41b55abc2914defcab7c6f28deee35e7f147ec5f46ac2b9983a670b24816f07dc1b6ec3648b0763a658b5003d60814311d56584713'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChromeYoutubeDl::Application.config.session_store :cookie_store, key: '_chrome-youtube-dl_session'
